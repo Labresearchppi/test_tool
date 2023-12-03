99ec093f0fba791ee0612aa44a76a643b7cce287
Sam Nieng <42174024+Labresearchppi@users.noreply.github.com>
2023-12-04 01:19:30 +0700

    Update test_tool.py

--- test_tool.py
+++ test_tool.py
@@ -438,7 +438,7 @@ class Main:
 	def old(self):
 		x = 111111111
 		xx = 999999999
-		idx = "100000" 
+		idx = "1000000" 
 		os.system('clear');print(logo)
 		limit = int(input(" \n\033[0;95m[+]\033[0;93m TOTAL IDS TO CRACK LIMIT 50,000: "))
 		try:
@@ -864,7 +864,7 @@ class Main:
 	def old2(self):
 		x = 1111111111
 		xx = 9999999999
-		idx = "10000" 
+		idx = "1000000" 
 		os.system('clear');print(logo)
 		limit = int(input("\n \033[0;95m[+]\033[0;93m TOTAL IDS TO CRACK LIMIT 50,000: "))
 		try:
